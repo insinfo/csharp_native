45ef1ddbada46039b596daeac721ea653b665e25
Isaque Neves <insinfo2008@gmail.com>
2022-05-04 15:54:50 -0300

    fix bugs on Targzip

--- Program.cs
+++ Program.cs
@@ -9,6 +9,9 @@ using System.Threading.Tasks;
 using ionicZip = Ionic.Zip;
 using Renci.SshNet;
 using Renci.SshNet.Sftp;
+using ICSharpCode.SharpZipLib.GZip;
+using ICSharpCode.SharpZipLib.Tar;
+using ICSharpCode.SharpZipLib.Core;
 
 namespace ConsoleApp1
 {
@@ -71,11 +74,11 @@ namespace ConsoleApp1
             DownloadFileListAsZip2(client, diff, @"C:\MyCsharpProjects\fsbackup\download_diff.zip");*/
             var watch = new System.Diagnostics.Stopwatch();
             watch.Start();
-            DownloadDirectory(client, "/var/www/dart/intranetbrowser", @"C:\MyCsharpProjects\fsbackup\download");
+            DownloadDirectoryAsTar(client, "/var/www/teste", @"C:\MyCsharpProjects\fsbackup\download.tar.gz");
             watch.Stop();
             Console.WriteLine($"End Download Execution Time: {watch.Elapsed} ms");
             client.Dispose();
-            
+
         }
         public static string PATHSEPARATOR = "/";
 
@@ -91,7 +94,7 @@ namespace ConsoleApp1
                 // Check if it is a file (not a directory).
                 if (!item.IsDirectory)
                 {
-                    Stream fileStream = File.Create(dp);                    
+                    Stream fileStream = File.Create(dp);
                     sftpClient.DownloadFile(sp, fileStream);
                     fileStream.Close();
                     fileStream.Dispose();
@@ -289,13 +292,17 @@ namespace ConsoleApp1
         {
             using (var zipFile = new FileStream(destLocalPath, FileMode.OpenOrCreate))
             {
-                using (var archive = new ZipArchive(zipFile, ZipArchiveMode.Create))//,leaveOpen:true
+                using (var gzoStream = new GZipOutputStream(zipFile))
+                using (var tarOutputStream = new TarOutputStream(gzoStream, null))
+                using (var tarArchive = TarArchive.CreateOutputTarArchive(tarOutputStream))
                 {
-                    DownloadDirectoryAsTarRec(archive, sftpClient, sourceRemotePath);
+                    tarArchive.RootPath = sourceRemotePath;                   
+                    DownloadDirectoryAsTarRec(tarOutputStream, sftpClient, sourceRemotePath);
+                    tarArchive.Close();
                 }
             }
         }
-        private static void DownloadDirectoryAsTarRec(ZipArchive archive, SftpClient sftpClient, string sourceRemotePath)
+        private static void DownloadDirectoryAsTarRec(TarOutputStream tarOut, SftpClient sftpClient, string sourceRemotePath)
         {
             try
             {
@@ -308,21 +315,33 @@ namespace ConsoleApp1
 
                         if (file.IsDirectory)
                         {
-                            DownloadDirectoryAsTarRec(archive, sftpClient, sourceFilePath);
+                            DownloadDirectoryAsTarRec(tarOut, sftpClient, sourceFilePath);
                         }
                         else
                         {
-                            var entry = archive.CreateEntry(sourceFilePath);
-                            Stream stream = entry.Open();
+                            //Stream stream = new MemoryStream();
                             try
                             {
-                                sftpClient.DownloadFile(sourceFilePath, stream);
-                                // entry.LastWriteTime = file.LastWriteTime;                               
-                                // using (var entryStream = entry.Open())
-                                // using (var fileStream = sftpClient.OpenRead(sourceFilePath))
-                                // {
-                                //fileStream.CopyTo(entryStream);
-                                //}
+
+                                //sftpClient.DownloadFile(sourceFilePath, stream);
+                                using (Stream stream = sftpClient.OpenRead(sourceFilePath))
+                                {
+                                    var entry = TarEntry.CreateTarEntry(sourceFilePath);
+                                    // var header = new TarHeader();
+                                    entry.TarHeader.LinkName = String.Empty;
+                                    entry.TarHeader.Mode = ExtractPermissions(file);//33216;
+                                    entry.TarHeader.TypeFlag = TarHeader.LF_NORMAL;// 48;
+                                    entry.TarHeader.Size = file.Attributes.Size;
+                                    entry.TarHeader.ModTime = DateTime.Now;
+                                    entry.TarHeader.DevMajor = 0;
+                                    entry.TarHeader.DevMinor = 0;
+                                    // archive.WriteEntry(entry,true);
+                                    tarOut.PutNextEntry(entry);
+                                    StreamUtils.Copy(stream, tarOut, new byte[4096]);
+                                    //tarOut.Write(array, 0, num);
+                                    tarOut.CloseEntry();
+                                }
+
                             }
                             catch (Exception e)
                             {
@@ -330,8 +349,8 @@ namespace ConsoleApp1
                             }
                             finally
                             {
-                                stream.Close();
-                                stream.Dispose();
+                                // stream.Close();
+                                // stream.Dispose();
                             }
 
                         }
@@ -342,6 +361,113 @@ namespace ConsoleApp1
             {
                 Console.WriteLine($"Download Directory failed: {sourceRemotePath} | Error:{e}");
             }
+        }
+
+        public static int ExtractPermissions(SftpFile file)
+        {
+            uint num = 0u;
+            /*  if (_isBitFiledsBitSet)
+              {
+                  num |= 0xF000;
+              }*/
+
+            if (file.IsSocket)
+            {
+                num |= 0xC000;
+            }
+
+            if (file.IsSymbolicLink)
+            {
+                num |= 0xA000;
+            }
+
+            if (file.IsRegularFile)
+            {
+                num |= 0x8000;
+            }
+
+            if (file.IsBlockDevice)
+            {
+                num |= 0x6000;
+            }
+
+            if (file.IsDirectory)
+            {
+                num |= 0x4000;
+            }
+
+            if (file.IsCharacterDevice)
+            {
+                num |= 0x2000;
+            }
+
+            if (file.IsNamedPipe)
+            {
+                num |= 0x1000;
+            }
+
+            /*if (_isUIDBitSet)
+            {
+                num |= 0x800;
+            }
+
+            if (file._isGroupIDBitSet)
+            {
+                num |= 0x400;
+            }
+
+            if (file._isStickyBitSet)
+            {
+                num |= 0x200;
+            }*/
+
+            if (file.OwnerCanRead)
+            {
+                num |= 0x100;
+            }
+
+            if (file.OwnerCanWrite)
+            {
+                num |= 0x80;
+            }
+
+            if (file.OwnerCanExecute)
+            {
+                num |= 0x40;
+            }
+
+            if (file.GroupCanRead)
+            {
+                num |= 0x20;
+            }
+
+            if (file.GroupCanWrite)
+            {
+                num |= 0x10;
+            }
+
+            if (file.GroupCanExecute)
+            {
+                num |= 8;
+            }
+
+            if (file.OthersCanRead)
+            {
+                num |= 4;
+            }
+
+            if (file.OthersCanWrite)
+            {
+                num |= 2;
+            }
+
+            if (file.OthersCanExecute)
+            {
+                num |= 1;
+            }
+
+            return (int)num  ;
+
         }
 
         public static List<FileModel> ListRemoteDirectory(SftpClient sftpClient, string sourceRemotePath)
